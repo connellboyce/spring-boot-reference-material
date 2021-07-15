153a13ea708eba220b4695aa7153285cb03892a3
Connell Boyce <connell.p.boyce@gmail.com>
2021-07-15 08:41:10 -0400

    Initialized basic Spring Boot file structure

---
+++ src/main/java/com/connellboyce/springbootreferencematerial/SpringBootReferenceMaterialApplication.java
@@ -0,0 +1,13 @@
+package com.connellboyce.springbootreferencematerial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootReferenceMaterialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootReferenceMaterialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/connellboyce/springbootreferencematerial/SpringBootReferenceMaterialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.connellboyce.springbootreferencematerial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootReferenceMaterialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
